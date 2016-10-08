ada09979518a281451c536d351a29c07d4cc3f54
mattkelsey <mattkelseymk@gmail.com>
2016-10-08 15:43:49 -0700

    Add proc loops

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/CameraOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/CameraOp.java
@@ -1,11 +1,5 @@
 package org.firstinspires.ftc.robotcontroller.internal;
 
-import org.firstinspires.ftc.robotcontroller.internal.CameraPreview;
-
-import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -18,14 +12,8 @@ import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
-import android.util.Log;
 
-/**
- * TeleOp Mode
- * <p>
- *Enables control of the robot via the gamepad
- */
-@TeleOp(name = "center vort4x", group = "Image Proc")
+@TeleOp(name = "center vortex", group = "Image Proc")
 public class CameraOp extends OpMode {
     private Camera camera;
     public CameraPreview preview;
@@ -79,9 +67,33 @@ public class CameraOp extends OpMode {
     public void loop() {
         if (yuvImage != null) {
             convertImage();
-            int pixel = image.getPixel(10, 10);
-            Color.blue(pixel);
-            
+
+            //assume on blue alliance
+            int threshold = 40; // how much farther above the average the blue value has to be to be tagged as blue
+            int totalBlue = 0;
+
+            for (int h = 0; h < height; h++){
+                for (int w = 0; w < width; w++) {
+                    totalBlue += Color.blue(image.getPixel(w, h));
+                }
+            }
+
+            int blueAvg = totalBlue/(width*height); // take
+            int[][] columnPixels = new int[width][2];
+            for (int w = 0; w < width; w++){
+                for (int h = 0; h < height; h++) {
+                    if (Color.blue(image.getPixel(w, h)) > blueAvg + threshold) {
+                        
+                        break;
+                    }
+                }
+                for (int h = height; h > height; h--) {
+                    break;
+                }
+
+
+            }
+
             String colorString = "";
 
             telemetry.addData("Color:", "Color detected is: " + colorString);
