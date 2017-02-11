651bf4bd354d6af46f609814b836ba4f6c4d7220
duncanmckee <duncan@mckee.net>
2017-02-11 10:44:02 -0800

    Add optical distance test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalDistance.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+/**
+ * Created by Duncan on 2/11/2017.
+ */
+
+@Autonomous(name = "Optical Distance")
+public class OpticalDistance extends LinearOpMode{
+
+    OpticalDistanceSensor ODS;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ODS = hardwareMap.opticalDistanceSensor.get("ods");
+
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addData("Light", ODS.getLightDetected() * 1024);
+            telemetry.update();
+        }
+    }
+}
