5c912266f54d3271fb2f8223acc198783936b03a
adamperlin <adamperlin@seattleacademy.org>
2017-03-13 21:46:33 -0700

    add changes to FancyCalibrator for calibration of  MR color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FancyCalibrator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FancyCalibrator.java
@@ -6,9 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cController;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
 import com.qualcomm.robotcore.hardware.configuration.I2cSensor;
 
 /**
@@ -18,9 +20,13 @@ import com.qualcomm.robotcore.hardware.configuration.I2cSensor;
 @Autonomous(name = "Calibrate Color Sensor")
 public class FancyCalibrator extends LinearOpMode {
 
-
+    public byte[] range1Cache;
+    public I2cDevice RANGE1;
+    public I2cDeviceSynch RANGE1Reader;
     @Override
     public void runOpMode() throws InterruptedException {
+        RANGE1 = hardwareMap.i2cDevice.get("r");
+
         ModernRoboticsI2cColorSensor cs = (ModernRoboticsI2cColorSensor) hardwareMap.colorSensor.get("bs");
         waitForStart();
         while(opModeIsActive()) {
@@ -29,6 +35,12 @@ public class FancyCalibrator extends LinearOpMode {
             cs.enableLed(false);
 
             telemetry.update();
+
+            range1Cache = RANGE1Reader.read(0x4, 2);
+
         }
+       RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, new I2cAddr(0x14), false);
+
+        RANGE1Reader.engage();
     }
 }
