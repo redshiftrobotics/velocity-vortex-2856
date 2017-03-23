809ffff9927756f1e4aa51c06abd3285be9a7e4b
Matt Kelsey <mattkelseymk@gmail.com>
2017-03-23 13:40:54 +0000

    Add easily tunable shooter aim constants class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterAim.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by matt on 3/23/17.
+ */
+
+public class ShooterAim {
+    public static float lob = 0.2f;
+    public static float far = 0.3f;
+    public static float medium = 0.4f;
+    public static float near = 0.55f;
+}
