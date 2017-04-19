e5c64b86ba5b6ee47707c3a9681a494d00b243c4
Matt Kelsey <mattkelseymk@gmail.com>
2017-04-19 04:27:36 +0000

    add servo calibration opmode for full chassis

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullChassisServoCalibrator.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by matt on 4/19/17.
+ */
+
+public class FullChassisServoCalibrator extends OpMode {
+
+    public Servo capServo1 = hardwareMap.servo.get("cap1");
+    public Servo capServo2 = hardwareMap.servo.get("cap2");
+
+    public boolean leftTriggerPressed = false;
+    public boolean leftBumperButtonPressed = false;
+
+    public boolean rightTriggerPressed = false;
+    public boolean rightBumperButtonPressed = false;
+
+    public float capServo1Position = 0.0f;
+    public float capServo2Position = 0.0f;
+
+
+
+    @Override
+    public void loop() {
+        if (gamepad1.left_trigger > 0.1 && !leftTriggerPressed) {
+            capServo1Position += 0.05;
+            leftTriggerPressed = true;
+        } else if(!(gamepad1.left_trigger>0.1)){
+            leftTriggerPressed = false;
+        }
+
+        if (gamepad1.left_bumper && !leftBumperButtonPressed) {
+            capServo1Position -= 0.05;
+            leftBumperButtonPressed = true;
+        } else if(!gamepad1.left_bumper){
+            leftBumperButtonPressed = false;
+        }
+
+        if (gamepad1.right_trigger > 0.1 && !rightTriggerPressed) {
+            capServo2Position += 0.05;
+            rightTriggerPressed = true;
+        } else if(!(gamepad1.right_trigger>0.1)){
+            rightTriggerPressed = false;
+        }
+
+        if (gamepad1.right_bumper && !rightBumperButtonPressed) {
+            capServo2Position -= 0.05;
+            rightBumperButtonPressed = true;
+        } else if(!gamepad1.right_bumper){
+            rightBumperButtonPressed = false;
+        }
+
+        capServo1Position = Range.clip(capServo1Position, 0, 1);
+        capServo2Position = Range.clip(capServo2Position, 0, 1);
+
+        capServo1.setPosition(capServo1Position);
+        capServo2.setPosition(capServo2Position);
+
+        telemetry.addData("Servo 1 ", capServo1Position);
+        telemetry.addData("Servo 2 ", capServo2Position);
+        telemetry.update();
+        
+
+    }
+
+    public void init() {
+
+    }
+}
