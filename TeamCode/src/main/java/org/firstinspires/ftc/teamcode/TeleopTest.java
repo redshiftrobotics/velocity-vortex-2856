b6678a06d1f67aee2a81ebbc41228017157048b4
Duncan McKee <duncan@mckee.net>
2017-04-28 20:11:03 -0700

    Add comments to TeleopTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTest.java
@@ -9,46 +9,50 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 
 /**
- * A test teleop program to control the robot with the DriveController.
+ * A test Teleop program to control the robot with the DriveController.
  * @author Duncan McKee
  */
 
 @TeleOp(name="Teleop Test")
 public class TeleopTest extends OpMode {
 
-    private DriveController driveController;
-    BNO055IMU imu;
+    private DriveController driveController; //Drive controller to control the robot's movements.
+    BNO055IMU imu; //A BNO055IMU used to determine the angle of the robot to use global movements.
 
-    private boolean aDebounce = false;
-    private boolean globalToggle = false;
-    private float savedAngle = 0;
-    private boolean initialized = false;
+    private boolean globalDeBounce = false; //A boolean used to update the robot from global to local movements.
+    private boolean globalToggle = false; //A boolean used to store which mode the robot's movements are in.
+    private float savedAngle = 0; //The current saved angle for local movements.
+    private boolean initialized = false; //A boolean to stop the OpMode if not all of the devices were initialized.
 
     @Override
     public void init() {
         telemetry.addData("IMU Initializing", 0);
         telemetry.update();
+
+        //Initialize the BNO055IMU to the specifications we use.
         I2cDeviceSynch imuInit;
         BNO055IMU.Parameters imuParameters;
         imuInit = hardwareMap.i2cDeviceSynch.get("imu");
         imuParameters = new BNO055IMU.Parameters();
         imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
         imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-
         imu = new AdafruitBNO055IMU(imuInit);
         imu.initialize(imuParameters);
+
         telemetry.addData("IMU Initialized", 1);
         telemetry.update();
 
-        driveController = new DriveController(DriveController.DriveType.Holonomic, hardwareMap);
-        initialized = true;
-        telemetry.addData("Everything is ready to go", 2);
+        driveController = new DriveController(DriveController.DriveType.Holonomic, hardwareMap); //Initialize the drive train in Holonomic mode.
+
+        initialized = true; //Set the status of the initialization process to true.
+
+        telemetry.addData("Everything is Ready to Go", 2);
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        if(!initialized){
+        if(!initialized){ //If the init function was not fully run stop the OpMode.
             requestOpModeStop();
         }
         Move(gamepad1);
@@ -57,11 +61,11 @@ public class TeleopTest extends OpMode {
     }
 
     void Move(Gamepad pad){
-        if(pad.a&&!aDebounce){
+        if(pad.a&&!globalDeBounce){
             globalToggle = !globalToggle;
-            aDebounce=true;
+            globalDeBounce=true;
         }else if(!pad.a){
-            aDebounce=false;
+            globalDeBounce=false;
         }
         if(pad.b){
             savedAngle = imu.getAngularOrientation().firstAngle * -1;
